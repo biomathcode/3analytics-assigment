c72f618d84b88158fd8b4b49d3f095a3df4bb6a6
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-12-08 23:22:24 +0530

    added login and logout

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,12 +1,35 @@
 import { Link, Outlet } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+
+type setUserType = (username:string, isLoggedIn: boolean) =>void;
+type toggleLogintype = (state: boolean) => void;
+
 
 function Navbar() {
+    function logOut (setUser:setUserType, toggleLogin: toggleLogintype) {
+        setUser('', false)
+        toggleLogin(false);
+    }
     return ( 
-        <nav>
-            <Link to="/">home</Link>
-            <Outlet />
-        </nav>
+        <UserContext.Consumer>
+
+            {({ isLoggedIn, user, setUser, toggleLogin }) => (
+                  
+                    <nav>
+                        <Link to="/">home</Link>
+                        {isLoggedIn ? 
+                        <button onClick={() =>logOut(setUser, toggleLogin)} >
+                        logOut
+                    </button>  :
+                        <Link to="/login">login</Link> 
+                      
+                    }
+                        
+                        <Outlet />
+                    </nav>
+                        )}
+        </UserContext.Consumer>
      );
 }
 
-export default Navbar;
+export default Navbar
